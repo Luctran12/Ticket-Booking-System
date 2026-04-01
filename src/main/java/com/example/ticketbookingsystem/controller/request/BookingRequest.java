17e2fad2ce1b664c9641ad8e7ba1232dc4da5a8e
luc12 <danunike687@gmail.com>
2026-04-01 16:28:55 +0700

    fix(BookingRequest): add validation on quantity

--- src/main/java/com/example/ticketbookingsystem/controller/request/BookingRequest.java
+++ src/main/java/com/example/ticketbookingsystem/controller/request/BookingRequest.java
@@ -1,9 +1,13 @@
 package com.example.ticketbookingsystem.controller.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class BookingRequest {
     private Long ticketId;
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Max(value = 10, message = "Cannot book more than 10 tickets at once")
     private Integer quantity;
 }
